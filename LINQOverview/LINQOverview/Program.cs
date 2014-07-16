594eecb845e463c7320d889319edd0eae30672f6
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-16 16:42:17 -0400

    More changes

--- LINQOverview/LINQOverview/Program.cs
+++ LINQOverview/LINQOverview/Program.cs
@@ -3,13 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LINQOverview
 {
+    public class Customer
+    {
+        public string CustomerID { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return CustomerID + "\t" + City;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            //NumQuery();
+            //ObjectQuery();
+            XMLQuery();
+        }
+
+        static IEnumerable<Customer> CreateCustomers()
+        {
+            //return new List<Customer>
+            //{
+            //    new Customer {CustomerID = "ALFKI", City = "Berlin"},
+            //    new Customer {CustomerID = "BONAP", City = "Marseille"},
+            //    new Customer {CustomerID = "CONSH", City = "London"},
+            //    new Customer {CustomerID = "EASTC", City = "London"},
+            //    new Customer {CustomerID = "FRANS", City = "Torino"},
+            //    new Customer {CustomerID = "LONEP", City = "Portlant"},
+            //    new Customer {CustomerID = "NORTS", City = "London"},
+            //    new Customer {CustomerID = "THEBI", City = "Portland"}
+            //};
+            return
+                from c in XDocument.Load("Customer.XML")
+                    .Descendants("Customers").Descendants()
+                select new Customer
+                {
+                    City = c.Attribute("City").Value,
+                    CustomerID = c.Attribute("CustomerID").Value
+                };
+        }
+        static void NumQuery()
+        {
+            var numbers = new int[] { 1, 4, 9, 16, 25, 36 };
+
+            var evenNumbers = from p in numbers
+                              where (p % 2) == 0
+                              select p;
+
+            Console.WriteLine("Result:");
+            foreach (var val in evenNumbers)
+                Console.WriteLine(val);
+        }
+
+        static void ObjectQuery()
+        {
+            var results = from c in CreateCustomers()
+                          where c.City == "London"
+                          select c;
+            foreach (var c in results)
+                Console.WriteLine(c);
+        }
+
+        public static void XMLQuery()
+        {
+            var doc = XDocument.Load("Customer.xml");
+
+            var results = from c in doc.Descendants("Customers")
+                          where c.Attribute("City").Value == "London"
+                          select c;
+
+            Console.WriteLine("Results:\n");
+            foreach (var contact in results)
+                Console.WriteLine("{0}\n", contact);
         }
     }
 }
